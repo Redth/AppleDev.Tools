8eaeef8b10daf95684ad1c81187afd4d4b3cb4f4
redth <jondick@gmail.com>
2023-04-13 17:11:49 -0400

    More file types in create secret command

--- AppleDev.Tool/Commands/CI/CreateSecretCommand.cs
+++ AppleDev.Tool/Commands/CI/CreateSecretCommand.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.Text.RegularExpressions;
-using AppleAppStoreConnect;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
@@ -8,31 +7,42 @@ namespace AppleDev.Tool.Commands;
 
 public class CreateSecretCommand : AsyncCommand<CreateSecretCommandSettings>
 {
-	public override async Task<int> ExecuteAsync(CommandContext context, CreateSecretCommandSettings settings)
+	public override Task<int> ExecuteAsync(CommandContext context, CreateSecretCommandSettings settings)
 	{
-		var data = context.GetData();
-
 		var secret = string.Empty;
 
 		if (settings.FromPrivateKey is not null)
-		{
-			var key = File.ReadAllText(settings.FromPrivateKey.FullName) ?? string.Empty;
-
-			secret = Regex.Replace(key, @"\s+", "");
-		}
-
-		if (settings.FromCertificate is not null)
-		{
-			var c = File.ReadAllBytes(settings.FromCertificate.FullName);
-
-			var b64 = Convert.ToBase64String(c);
-
-			secret = Regex.Replace(b64, @"\s+", "");
-		}
+			secret = FromTextFile(settings.FromPrivateKey);
+		else if (settings.FromTextFile is not null)
+			secret = FromTextFile(settings.FromTextFile);
+		else if (settings.FromCertificate is not null)
+			secret = FromBinaryFile(settings.FromCertificate);
+		else if (settings.FromKeystore is not null)
+			secret = FromBinaryFile(settings.FromKeystore);
+		else if (settings.FromPepk is not null)
+			secret = FromBinaryFile(settings.FromPepk);
+		else if (settings.FromBinaryFile is not null)
+			secret = FromBinaryFile(settings.FromBinaryFile);
 
 		AnsiConsole.WriteLine(secret);
 
-		return this.ExitCode();
+		return Task.FromResult(this.ExitCode());
+	}
+
+	string FromTextFile(FileInfo file)
+	{
+		var key = File.ReadAllText(file.FullName) ?? string.Empty;
+
+		return Regex.Replace(key, @"\s+", "");
+	}
+
+	string FromBinaryFile(FileInfo file)
+	{
+		var c = File.ReadAllBytes(file.FullName);
+
+		var b64 = Convert.ToBase64String(c);
+
+		return Regex.Replace(b64, @"\s+", "");
 	}
 }
 public class CreateSecretCommandSettings : CommandSettings
@@ -44,4 +54,20 @@ public class CreateSecretCommandSettings : CommandSettings
 	[Description("Private Key file (.p8)")]
 	[CommandOption("--from-private-key <private_key_file>")]
 	public FileInfo? FromPrivateKey { get; set; }
+
+	[Description("Keystore file (.keystore)")]
+	[CommandOption("--from-keystore <keystore_file>")]
+	public FileInfo? FromKeystore { get; set; }
+
+	[Description("Google PEPK Key file (.pepk)")]
+	[CommandOption("--from-pepk <pepk_key_file>")]
+	public FileInfo? FromPepk { get; set; }
+
+	[Description("Text file")]
+	[CommandOption("--from-text-file <text_file>")]
+	public FileInfo? FromTextFile { get; set; }
+
+	[Description("Binary file")]
+	[CommandOption("--from-binary-file <binary_file>")]
+	public FileInfo? FromBinaryFile { get; set; }
 }
