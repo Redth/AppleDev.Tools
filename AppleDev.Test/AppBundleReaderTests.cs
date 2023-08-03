d4bae9e65e62c5cf5bcbbb4cad776c039fd24e5c
redth <jondick@gmail.com>
2023-08-03 15:06:29 -0400

    Add AppBundleReader
    To help parse app bundle info plist

---
+++ AppleDev.Test/AppBundleReaderTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace AppleDev.Test;
+
+public class AppBundleReaderTests : TestsBase
+{
+	public AppBundleReaderTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+	{
+		AppFile = Path.Combine(TestDataDirectory, "MauiApp7.app");
+	}
+
+	public readonly string AppFile;
+
+	[Fact]
+	public void ReadBundleId()
+	{
+		var reader = new AppBundleReader(AppFile);
+
+		var infoplist = reader.ReadInfoPlist();
+
+		var v = infoplist.CFBundleIdentifier;
+
+		Assert.Equal("com.companyname.mauiapp12345", v);
+	}
+
+
+	[Fact]
+	public void ReadBundleVersion()
+	{
+		var reader = new AppBundleReader(AppFile);
+
+		var infoplist = reader.ReadInfoPlist();
+
+		var v = infoplist.CFBundleVersion;
+
+		Assert.Equal("1", v);
+	}
+
+	[Fact]
+	public void ReadBundleShortVersion()
+	{
+		var reader = new AppBundleReader(AppFile);
+
+		var infoplist = reader.ReadInfoPlist();
+
+		var v = infoplist.CFBundleShortVersionString;
+
+		Assert.Equal("1.0", v);
+	}
+}
---
+++ AppleDev/AppBundleReader.cs
@@ -0,0 +1,76 @@
+﻿using Claunia.PropertyList;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleDev;
+
+public class AppBundleReader
+{
+	public AppBundleReader(string appFilename)
+	{
+		if (!File.Exists(appFilename) && !Directory.Exists(appFilename))
+			throw new ArgumentNullException(nameof(appFilename));
+
+		if (appFilename.EndsWith(".app", StringComparison.InvariantCultureIgnoreCase))
+		{
+			// iOS is in root of .app
+			InfoPlistFile = Path.Combine(appFilename.TrimEnd('/') + "/", "Info.plist");
+
+			// If not there, check for mac pattern inside Contents/Info.plist
+			if (!File.Exists(InfoPlistFile))
+				InfoPlistFile = Path.Combine(appFilename.TrimEnd('/') + "/", "Contents", "Info.plist");
+		}
+	}
+
+	public readonly string? InfoPlistFile;
+
+	public AppInfoPlist ReadInfoPlist()
+	{
+		if (!File.Exists(InfoPlistFile))
+			throw new Exception($"Could not find Info.plist in {InfoPlistFile}");
+
+		return new AppInfoPlist(InfoPlistFile);
+	}
+}
+
+public class AppInfoPlist
+{
+	public AppInfoPlist(string infoPlistFile)
+	{
+		Filename = infoPlistFile;
+		Root = PropertyListParser.Parse(Filename) as NSDictionary;
+	}
+
+	public readonly string Filename;
+
+	public readonly NSDictionary? Root;
+
+	public string? CFBundleVersion
+		=> Root?.ObjectForKey("CFBundleVersion")?.ToString();
+
+	public string? CFBundleShortVersionString
+		=> Root?.ObjectForKey("CFBundleShortVersionString")?.ToString();
+
+	public string? CFBundleDisplayName
+		=> Root?.ObjectForKey("CFBundleDisplayName")?.ToString();
+
+	public string? CFBundleName
+		=> Root?.ObjectForKey("CFBundleName")?.ToString();
+
+	public string? CFBundleIdentifier
+		=> Root?.ObjectForKey("CFBundleIdentifier")?.ToString();
+
+	public string? DTPlatformName
+		=> Root?.ObjectForKey("DTPlatformName")?.ToString();
+
+	public string? DTPlatformVersion
+		=> Root?.ObjectForKey("DTPlatformVersion")?.ToString();
+
+	public string? DTSDKName
+		=> Root?.ObjectForKey("DTSDKName")?.ToString();
+}
