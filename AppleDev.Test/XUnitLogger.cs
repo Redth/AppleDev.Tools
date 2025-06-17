aef2246b084dcc2493a8050e894dd6a05bdc8876
Matthew Leibowitz <mattleibow@live.com>
2025-06-18 00:42:54 +0200

    Update XUnitLogger.cs

--- AppleDev.Test/XUnitLogger.cs
+++ AppleDev.Test/XUnitLogger.cs
@@ -4,31 +4,31 @@ using Xunit.Abstractions;
 namespace AppleDev.Test;
 
 class XUnitLogger<T> : ILogger<T>
+{
+	private readonly ITestOutputHelper _testOutputHelper;
+
+	public XUnitLogger(ITestOutputHelper testOutputHelper)
 	{
-		private readonly ITestOutputHelper _testOutputHelper;
+		_testOutputHelper = testOutputHelper;
+	}
 
-		public XUnitLogger(ITestOutputHelper testOutputHelper)
+	public IDisposable? BeginScope<TState>(TState state)
+		where TState : notnull
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool IsEnabled(LogLevel logLevel) => true;
+
+	public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+	{
+		var message = formatter(state, exception);
+
+		_testOutputHelper.WriteLine($"[{logLevel}] {message}");
+
+		if (exception != null)
 		{
-			_testOutputHelper = testOutputHelper;
-		}
-
-		public IDisposable? BeginScope<TState>(TState state)
-			where TState : notnull
-		{
-			throw new NotImplementedException();
-		}
-
-		public bool IsEnabled(LogLevel logLevel) => true;
-
-		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-		{
-			var message = formatter(state, exception);
-
-			_testOutputHelper.WriteLine($"[{logLevel}] {message}");
-
-			if (exception != null)
-			{
-				_testOutputHelper.WriteLine(exception.ToString());
-			}
+			_testOutputHelper.WriteLine(exception.ToString());
 		}
 	}
+}
